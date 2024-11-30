3330e40a5893d963a1d7f91f3c3c3cf6bfb232a8
Kirt Henrie <sandnap@gmail.com>
2024-11-30 12:17:55 -0700

    Fixed linting errors

--- app/models/note.rb
+++ app/models/note.rb
@@ -11,7 +11,7 @@ class Note < ApplicationRecord
 
   private
 
-  def set_default_importance
-    self.importance ||= 5
-  end
+    def set_default_importance
+      self.importance ||= 5
+    end
 end

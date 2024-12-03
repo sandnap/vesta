fcd024331d84f001b7708c6dfd8270f91354b7e1
Kirt Henrie <sandnap@gmail.com>
2024-12-03 10:10:18 -0700

    Fixed theme icon

--- app/javascript/controllers/theme_controller.js
+++ app/javascript/controllers/theme_controller.js
@@ -3,17 +3,29 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="theme"
 export default class extends Controller {
   connect() {
+    const darkIcon = this.element.querySelector("#theme-toggle-dark-icon")
+    const lightIcon = this.element.querySelector("#theme-toggle-light-icon")
+
     if (
       localStorage.getItem("color-theme") === "dark" ||
       (!("color-theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)
     ) {
       document.documentElement.classList.add("dark")
+      lightIcon.classList.remove("hidden")
     } else {
       document.documentElement.classList.remove("dark")
+      darkIcon.classList.remove("hidden")
     }
   }
 
   toggle() {
+    const darkIcon = this.element.querySelector("#theme-toggle-dark-icon")
+    const lightIcon = this.element.querySelector("#theme-toggle-light-icon")
+
+    // toggle icons
+    darkIcon.classList.toggle("hidden")
+    lightIcon.classList.toggle("hidden")
+
     // Toggle theme
     if (localStorage.getItem("color-theme")) {
       if (localStorage.getItem("color-theme") === "light") {

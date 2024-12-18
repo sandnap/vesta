f64d941f645d3a3dba14a13a9e96943b2f452202
Kirt Henrie <sandnap@gmail.com>
2024-12-18 08:22:25 -0700

    Finished PWA config

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+
   # Authentication routes
   resource :session
   resources :passwords, param: :token
